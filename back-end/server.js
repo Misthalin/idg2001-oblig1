5efedcf7d9b03a49c7b875da03d11eb12ced2dfc
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-02-24 14:25:18 +0100

    Server shell

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const env = require('dotenv').config();
 
 const app = express()
+const PORT = process.env.PORT || 8080
 
+app.get("/", (req, res) => {
+    res.send("Server running")
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
