0039febbd0cf0fa9a8dcf06586cfe65d15d8d5aa
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-02-24 14:32:04 +0100

    server render front-end

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const env = require('dotenv').config();
+const path = require('path');
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-    res.send("Server running")
+    res.sendFile(path.join(__dirname, "../front-end", "index.html"))
 })
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
