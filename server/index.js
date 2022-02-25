1a61f78775b110e7fe38fc5eae361cce6781a656
Adrian Riise <adrianriise@dhcp-10-22-196-116.wlan.ntnu.no>
2022-02-25 11:24:29 +0100

    different structure

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path')
+const app = express();
+require('./database');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// API
+const students = require('./api/students');
+app.use('/api/students', students);
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
