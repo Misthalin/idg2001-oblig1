9b9f79c88942c9fb54c0442973cc7d8ba98ebdef
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-03-04 15:03:21 +0100

    server cleanup

--- server/index.js
+++ server/index.js
@@ -18,6 +18,4 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 5050;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
