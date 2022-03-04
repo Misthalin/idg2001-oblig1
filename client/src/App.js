89e84a9370bfdb8c209fd574f3169b9a1f54eb32
Adrian Riise <adrianriise@Adrians-MacBook-Pro.local>
2022-03-04 16:53:15 +0100

    Added form confirmation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css'
 import AppliedStudents from './components/AppliedStudents';
 import FormSubmittions from './components/FormSubmittion';
 
 const App = function () {
 	return (
-		<div>
-		<h1>Apply for Course: IDG1362 User-Centered Design</h1>
+		<div class="body-margin">
+		<h1 class="text-center">Course Registration</h1>
+		<p class="text-center">Apply for Course: IDG1362 User-Centered Design</p>
 			<FormSubmittions />
 			<AppliedStudents />
 		</div>
